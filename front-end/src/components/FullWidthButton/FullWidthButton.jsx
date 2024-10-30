08f875eecf91ffe0ddb7b94a0db884aa025c507b
Jakub Wielgocki <jakubwielgocki02@gmail.com>
2024-10-30 01:07:30 +0100

    Added view ProductDetails

--- front-end/src/components/FullWidthButton/FullWidthButton.jsx
+++ front-end/src/components/FullWidthButton/FullWidthButton.jsx
@@ -3,7 +3,7 @@ import styles from "./FullWidthButton.module.css";
 export function FullWidthButton({ children, onClick, isBlack }) {
 	return (
 		<button
-			className={`${styles.button} ${isBlack ? styles.isBlack : ""}`}
+			className={`${styles.button} ${isBlack ? styles.black : ""}`}
 			onClick={onClick}>
 			{children}
 		</button>
