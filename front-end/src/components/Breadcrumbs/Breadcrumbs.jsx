9916c6035a1a84fb5d0c5dcac999c56aa70d2365
Jakub Wielgocki <jakubwielgocki02@gmail.com>
2024-11-21 01:49:35 +0100

    Fixed breadcrumbs

--- front-end/src/components/Breadcrumbs/Breadcrumbs.jsx
+++ front-end/src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,29 +1,44 @@
 import styles from "./Breadcrumbs.module.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import ARROW_ICON from "../../assets/arrow.svg";
+import { CATEGORIES, GENDERS } from "../../constants/categories";
 
 export function Breadcrumbs() {
+	const { gender, category, subcategory } = useParams();
+
+	const foundGender = GENDERS.find((g) => g.path === gender);
+	const foundCategory =
+		foundGender && CATEGORIES.find((c) => c.path === category);
+
 	const breadcrumbs = [
 		{
-			categoryName: "Kobieta",
-			path: "kobieta",
+			categoryName: foundGender.categoryName,
+			path: `/${foundGender.path}`,
 		},
 		{
-			categoryName: "Odzież",
-			path: "odziez",
-		},
-		{
-			categoryName: "Swetry",
-			path: "swetry",
+			categoryName: foundCategory.categoryName,
+			path: `/${foundGender.path}/${foundCategory.path}`,
 		},
 	];
+
+	let foundSubcategory;
+	if (subcategory && foundCategory) {
+		foundSubcategory = foundCategory.subcategories.find(
+			(sc) => sc.path === subcategory
+		);
+
+		breadcrumbs.push({
+			categoryName: foundSubcategory.categoryName,
+			path: `/${foundGender.path}/${foundCategory.path}/${foundCategory.path}`,
+		});
+	}
 
 	return (
 		<ul className={styles.breadcrumbs}>
 			{breadcrumbs.map((breadcrumb) => {
 				return (
 					<li key={breadcrumb.path}>
-						<NavLink to={breadcrumb.path}>
+						<NavLink end to={breadcrumb.path}>
 							{breadcrumb.categoryName}
 							<img src={ARROW_ICON} alt='' />
 						</NavLink>
